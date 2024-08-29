10ce37f7735f63f8dfd578a8e51721e55c2673b0
LoianSous <loiansouza@outlook.com>
2024-08-29 15:31:36 -0400

    nadademais

--- principal/script.js
+++ principal/script.js
@@ -19,44 +19,41 @@ function mostrarTarefas() {
 
     let novoCard = ''
 
-    lista.forEach( (item, posicao) => {
-
-        novoCard = novoCard + `
-        
-        <div class="card task-card">
-                <img src="#" class="card-img-top" alt="Imagem de Prioridade">
-                <div class="card-body">
-                    <div>
-                        <h5 class="card-title">${item.tarefa}</h5>
-                        <p class="card-text">Prioridade:</p>
-                    </div>
-                    <div class="task-buttons">
-                        <button class="btn btn-success">
-                            <box-icon name='check-double' color="white" onclick="concluirTarefa(${posicao})"></box-icon>
-                        </button>
-                        <button class="btn btn-warning">
-                            <box-icon name='edit' color="white"></box-icon>
-                        </button>
-                        <button class="btn btn-danger">
-                            <box-icon name='trash' color="white" onclick="deletarTarefa(${posicao})"></box-icon>
-                        </button>
-                    </div>
+    lista.forEach((item, posicao) => {
+        novoCard += `
+        <div class="card task-card ${item.concluida ? "feito" : ""}">
+            <img src="#" class="card-img-top" alt="Imagem de Prioridade">
+            <div class="card-body">
+                <div>
+                    <h5 class="card-title">${item.tarefa}</h5>
+                    <p class="card-text">Prioridade:</p>
+                </div>
+                <div class="task-buttons">
+                    <button class="btn btn-success" onclick="concluirTarefa(${posicao})">
+                        <box-icon name='check-double' color="white"></box-icon>
+                    </button>
+                    <button class="btn btn-warning">
+                        <box-icon name='edit' color="white"></box-icon>
+                    </button>
+                    <button class="btn btn-danger" onclick="deletarTarefa(${posicao})">
+                        <box-icon name='trash' color="white"></box-icon>
+                    </button>
                 </div>
             </div>
-
+        </div>
         `
-
-        listaCompleta.innerHTML = novoCard
     })
+
+    listaCompleta.innerHTML = novoCard
 }
 
 function concluirTarefa(posicao){
     lista[posicao].concluida = !lista[posicao].concluida
+    mostrarTarefas()
 }
 
 function deletarTarefa(posicao){
     lista.splice(posicao, 1)
-
     mostrarTarefas()   
 }
 
