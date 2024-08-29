0cb1e9b2b66c5b4695a3302c01306872595943cc
LoianSous <loiansouza@outlook.com>
2024-08-29 16:59:02 -0400

    finalizado

--- principal/script.js
+++ principal/script.js
@@ -1,25 +1,28 @@
-const botao = document.getElementById("addTaskBtn")
-const texto = document.getElementById("taskInput")
-const listaCompleta = document.getElementById("taskList")
-const prioridade_tarefa = document.getElementById("priorityInput")
-const filtro_texto = document.getElementById("filterInput")
-const filtro = document.getElementById("filterPriority")
-
+const botao = document.getElementById("addTarefaBtn")
+const texto = document.getElementById("tarefaInput")
+const listaCompleta = document.getElementById("listaTarefas")
+const prioridade_tarefa = document.getElementById("prioridadeInput")
+const filtro_texto = document.getElementById("filtroInput")
+const filtro = document.getElementById("filtroPrioridade")
 
 let lista = []
 let itensAMostra = []
 
+const imagemprioridade = {
+    'Alto': 'imagens/alta.png',
+    'Médio': 'imagens/media.png', 
+    'Baixo': 'imagens/baixa.png'
+}
+
 function AdicionarNovaTarefa() {
-    const prioridade = priorityInput.value;
+    const prioridade = prioridade_tarefa.value;
     const itemNovo = {
         tarefa: texto.value,
         prioridade: prioridade,
-        filtro: prioridade_tarefa,
         concluida: false
     }
 
     lista.push(itemNovo)
-
     itensAMostra.push(itemNovo)
 
     texto.value = ''
@@ -28,77 +31,80 @@ function AdicionarNovaTarefa() {
 }
 
 function mostrarTarefas() {
-
     let novoCard = ''
 
-    lista.forEach( (item, posicao) => {
+    itensAMostra.forEach((item, posicao) => {
 
-        novoCard = novoCard + `
-        
-        <div class="card task-card">
-                <img src="#" class="card-img-top" alt="Imagem de Prioridade">
-                <div class="card-body">
-                    <div>
-                        <h5 class="card-title">${item.tarefa}</h5>
-                        <p class="card-text">Prioridade:</p>
-                    </div>
-                    <div class="task-buttons">
-                        <button class="btn btn-success">
-                            <box-icon name='check-double' color="white" onclick="concluirTarefa(${posicao})"></box-icon>
-                        </button>
-                        <button class="btn btn-warning">
-                            <box-icon name='edit' color="white"></box-icon>
-                        </button>
-                        <button class="btn btn-danger">
-                            <box-icon name='trash' color="white" onclick="deletarTarefa(${posicao})"></box-icon>
-                        </button>
-                    </div>
+        let imagem = imagemprioridade[item.prioridade]
+
+        novoCard += `
+        <div class="card task-card ${item.concluida ? "feito" : ""}">
+            <img src="${imagem}" class="card-img-top" alt="Imagem de Prioridade">
+            <div class="card-body">
+                <div>
+                    <h5 class="card-title">${item.tarefa}</h5>
+                    <p class="card-text">Prioridade: ${item.prioridade}</p>
+                </div>
+                <div class="task-buttons">
+                    <button class="btn btn-success" onclick="concluirTarefa(${posicao})">
+                        <box-icon name='check-double' color="white"></box-icon>
+                    </button>
+                    <button class="btn btn-warning">
+                        <box-icon name='edit' color="white"></box-icon>
+                    </button>
+                    <button class="btn btn-danger" onclick="deletarTarefa(${posicao})">
+                        <box-icon name='trash' color="white"></box-icon>
+                    </button>
                 </div>
             </div>
         </div>
         `
-
-        listaCompleta.innerHTML = novoCard
     })
 
     listaCompleta.innerHTML = novoCard
+
+    localStorage.setItem('lista', JSON.stringify(lista))
 }
 
-function concluirTarefa(posicao){
-    lista[posicao].concluida = !lista[posicao].concluida
+function concluirTarefa(posicao) {
+    itensAMostra[posicao].concluida = !itensAMostra[posicao].concluida
+    mostrarTarefas()
 }
 
-function deletarTarefa(posicao){
+function deletarTarefa(posicao) {
     lista.splice(posicao, 1)
-
-    mostrarTarefas()   
+    itensAMostra = lista.slice();  
+    mostrarTarefas()
 }
 
-function recarregarTarefa(){
+function recarregarTarefas() {
     const tarefa_armazenada = localStorage.getItem('lista')
-
     if (tarefa_armazenada) {
         lista = JSON.parse(tarefa_armazenada)
+        itensAMostra = lista.slice(); 
     }
-
-
     mostrarTarefas()
 }
 
-function filtrarTarefas(){
+function filtrarTarefas() {
     const textoFiltro = filtro_texto.value.toLowerCase();
-    const valorPrioridade= prioridade_tarefa.value;
+    const valorPrioridade = filtro.value;
 
-    
-    if (textoFiltro || valorPrioridade != "Todos"){
-        itensAMostra = lista.filter(lista => lista.tarefa.toLowerCase().includes(textoFiltro));
-       // itensAMostra = itensPorNome.filter(lista => lista.prioridade == valorPrioridade); 
+    itensAMostra = lista.filter(item => item.tarefa.toLowerCase().includes(textoFiltro));
+
+    if (valorPrioridade !== "Todos") {
+        itensAMostra = itensAMostra.filter(item => item.prioridade === valorPrioridade);
+    }
+
+    if (!textoFiltro && valorPrioridade === "Todos") {
+        itensAMostra = lista.slice();  
     }
 
     mostrarTarefas()
-
-    
 }
 
-recarregarTarefa()
+recarregarTarefas()
 botao.addEventListener('click', AdicionarNovaTarefa)
+filtro_texto.addEventListener('input', filtrarTarefas)
+filtro.addEventListener('change', filtrarTarefas)
+
